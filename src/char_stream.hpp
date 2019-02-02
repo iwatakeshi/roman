292acda0cfba8088c4c0e7601a4a16545b94c530
Takeshi <iwatakeshi@gmail.com>
2019-02-01 20:17:28 -0700

    Update char_stream.hpp
    Add logic enhancements

--- src/char_stream.hpp
+++ src/char_stream.hpp
@@ -9,32 +9,38 @@ private:
   uint64_t position_ = 0;
 public:
   CharStream() {}
-  CharStream(std::string source) {
+  CharStream(const std::string& source) {
     source_ = source;
   }
   ~CharStream() {
     source_ = "";
   }
 
-  const char& previous() {
+  const char previous() {
     if (position_ < 0) {
-      throw "position_ out of bounds.";
+      throw "position out of bounds.";
     }
-    return source_[position_--];
-  }
-  
-  const char& next() {
-    if (position_ >= source_.length()) {
-      throw "position_ out of bounds.";
-    }
-    return source_[position_++];
+    position_--;
+    return std::tolower(source_[position_ + 1]);
   }
 
-  const char& peek(int64_t by) {
-    if ((by + position_) < 0 || (by + position_) >= source_.length()) {
-      throw "position_ out of bounds.";
+  const char current() {
+    return std::tolower(source_[position_]);
+  }
+  
+  const char next() {
+    if (position_ >= source_.length()) {
+      throw "position out of bounds.";
     }
-    return source_[by + position_];
+    position_++;
+    return std::tolower(source_[position_ - 1]);
+  }
+
+  const char peek(int64_t by) {
+    if ((by + position_) < 0 || (by + position_) >= source_.length()) {
+      throw "position out of bounds.";
+    }
+    return std::tolower(source_[by + position_]);
   }
 
   bool is_eos() {
@@ -43,6 +49,20 @@ public:
 
   std::string to_string() {
     return source_;
+  }
+
+  std::string range(int start) {
+    return range(start, position());
+  }
+
+  std::string range(int start, int end) {
+    // Implemented my own substring method
+    // because std's is buggy(?)
+    std::string temp = "";
+    for(auto i = start; i < end; i++) {
+     temp += std::tolower(source_[i]);
+    }
+    return temp;
   }
 
   uint64_t position() {
